4ff03b7fb0a1da6916a4dc5e41a50cb71bac942c
Josiah Kruse <sik645@gmail.com>
2024-02-19 21:21:36 -0800

    Add mobile optimized image

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.scss';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -31,7 +32,22 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          {/* Mobile Hero Image */}
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="md:hidden"
+            alt="Screenshots of the project"
+          />
+          {/* Desktop Hero Image */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the project"
+          />
         </div>
       </div>
     </main>
